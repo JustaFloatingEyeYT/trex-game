0b156c5a935d4ca88dd8d7921e70c6b1a5513080
JustaFloatingEyeYT <79027427+JustaFloatingEyeYT@users.noreply.github.com>
2022-01-18 22:06:26 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,27 @@
 //Declaring global variables
-var trex ,trex_running;
+var trex,trex_running;
 var ground,groundImage;
 var groundColliding;
+var cloud,cloudImage;
+var obstacle,obstacleImage1;
+var obstacleImage2;
+var obstacleImage3;
+var obstacleImage4;
+var obstacleImage5;
+var obstacleImage6;
+var score = 0;
 
 //Predifined function used to load things
 function preload(){
   trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
   groundImage = loadImage("ground2.png");
+  cloudImage = loadImage("cloud.png");
+  obstacleImage1 = loadImage("obstacle1.png");
+  obstacleImage2 = loadImage("obstacle2.png");
+  obstacleImage3 = loadImage("obstacle3.png");
+  obstacleImage4 = loadImage("obstacle4.png");
+  obstacleImage5 = loadImage("obstacle5.png");
+  obstacleImage6 = loadImage("obstacle6.png");
 }
 
 //To create things, only happens once
@@ -26,17 +41,21 @@ function setup(){
   //Creating invisible ground
   groundColliding = createSprite(300,189,600,10);
   groundColliding.visible = false
+
 }
 
 //Does things for every frame
 function draw(){
   //Changes the backround and stops duplicating sprites when they move
   background("white");
+
+  text("Score: " + score, 10, 15);
+  score = Math.round(frameCount / 4);
   if(keyDown("space") && trex.collide(ground)){
-    trex.velocityY = -6;
+    trex.velocityY = -16;
   }
   //Adding gravity
-  trex.velocityY += 0.2;
+  trex.velocityY += 1.60;
 
   //Stopping the trex from falling out of the game
   trex.collide(groundColliding)
@@ -45,6 +64,45 @@ function draw(){
   if(ground.x < 0){
     ground.x = 1100;
   }
+
+  obstacleSpawn();
+  spawnClouds();
   drawSprites();
 
+}
+
+function spawnClouds(){
+  if(frameCount%60 == 0){
+    cloud = createSprite(645,80,2,2)
+    cloud.addImage("clouds",cloudImage)
+    cloud.velocityX = -5
+    cloud.y = Math.round(random(0,100))
+    trex.depth = cloud.depth;
+    cloud.lifetime = 140;
+  }
+}
+
+function obstacleSpawn(){
+  if(frameCount%60 == 0){
+    obstacle = createSprite(645,160,10,10);
+    obstacle.velocityX = -10;
+    var r = Math.round(random(1,6));
+    obstacle.lifetime = 140;
+    switch(r){
+      case 1: obstacle.addImage(obstacleImage1); 
+      break;
+      case 2: obstacle.addImage(obstacleImage2); 
+      break;
+      case 3: obstacle.addImage(obstacleImage3); 
+      break;
+      case 4: obstacle.addImage(obstacleImage4); 
+      break;
+      case 5: obstacle.addImage(obstacleImage5); 
+      break;
+      case 6: obstacle.addImage(obstacleImage6); 
+      break;
+      default: break;
+    }
+    obstacle.scale = 0.5;
+  }
 }
